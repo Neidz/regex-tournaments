4f7aabdfe08a58c1c24e0ab6a20bde21259820ab
Neidz <adamo7255@gmail.com>
2023-01-23 22:49:59 +0100

    .

---
+++ src/lib/Header.svelte
@@ -0,0 +1,10 @@
+<style lang="scss">
+   h1 {
+    color: var(--text-color)
+   } 
+   span {
+    color: var(--main-color)
+   }
+</style>
+
+<h1>regex<span>tournaments</span></h1>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,17 @@
+<script>
+	import Header from "../lib/Header.svelte";
+
+</script>
+<style lang="scss">
+    :global(:root){
+    --bg-color: #323437;
+    --main-color: #1489e2;
+    --sub-color: #646669;
+    --text-color: #d1d0c5;
+    --error-color: #ca4754;
+    }
+
+</style>
+
 <h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<Header />
